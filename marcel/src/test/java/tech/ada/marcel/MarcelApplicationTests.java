9af433600c3b531ba93389b7aef01b8fb62bdab6
cinbrito <cin.brito@outlook.com>
2024-04-22 20:57:53 -0300

    initial commit

---
+++ marcel/src/test/java/tech/ada/marcel/MarcelApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.marcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
